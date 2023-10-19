c28fbdbf6a3f7e06220697f2d49e9d40dcf5c200
sahago <sahapanova@yandex.ru>
2023-10-20 01:52:03 +0300

    Task 5.1 added

--- Stepik_Algorithms_-_theory_and_practice._Data_structures/5.1_Phonebook/5.1_Phonebook.go
+++ Stepik_Algorithms_-_theory_and_practice._Data_structures/5.1_Phonebook/5.1_Phonebook.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"bufio"
+	"crypto/md5"
 	"fmt"
-	"io"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -12,15 +13,21 @@ type PhoneBook struct {
 	records map[string]string
 }
 
+// AddNumberName добавление в записную книжку
 func (pb *PhoneBook) AddNumberName(num string, name string) {
+	num = pb.HashFunc(num)
 	pb.records[num] = name
 }
 
+// DeleteNumber удалить номер
 func (pb *PhoneBook) DeleteNumber(num string) {
+	num = pb.HashFunc(num)
 	delete(pb.records, num)
 }
 
+// FindNumber найти номер
 func (pb *PhoneBook) FindNumber(num string) {
+	num = pb.HashFunc(num)
 	if v, ok := pb.records[num]; ok {
 		fmt.Fprintln(Out, v)
 		return
@@ -29,37 +36,35 @@ func (pb *PhoneBook) FindNumber(num string) {
 	return
 }
 
+// HashFunc хешировать по алгоритму МД5
+func (pb *PhoneBook) HashFunc(num string) string {
+	key := []byte(num)
+	return fmt.Sprintf("%x", md5.Sum(key))
+}
+
 type Methods interface {
+	HashFunc(string) string
 	AddNumberName(string, string)
 	DeleteNumber(string)
 	FindNumber(string)
 }
 
 var (
-	In  = bufio.NewReader(os.Stdin)
-	Out = bufio.NewWriter(os.Stdout)
+	Scan = bufio.NewScanner(os.Stdin)
+	Out  = bufio.NewWriter(os.Stdout)
 )
 
 func main() {
 	defer Out.Flush()
-	var request int
-	fmt.Fscanln(In, &request)
+	Scan.Scan()
+	request, _ := strconv.Atoi(Scan.Text())
 
 	phoneBook := CreatePhoneBook()
 
 	for i := 0; i < request; i++ {
 
-		line, err := In.ReadString('\n')
-		if err != nil {
-			if err == io.EOF {
-				break
-			} else {
-				fmt.Println(err)
-				return
-			}
-		}
-		line = strings.Trim(line, "\n")
-		buff := strings.Split(line, " ")
+		Scan.Scan()
+		buff := strings.Split(Scan.Text(), " ")
 
 		num := buff[1]
 
@@ -73,11 +78,10 @@ func main() {
 		default:
 			fmt.Fprintln(Out, "how does it happen?")
 		}
-
 	}
-
 }
 
+// CreatePhoneBook создать записную книжку
 func CreatePhoneBook() PhoneBook {
 	pB := PhoneBook{}
 	pB.records = make(map[string]string)
